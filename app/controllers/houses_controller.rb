7b72e8310f4a4d0946dec9f06f03f1c83142f507
Gonzalo Cobos Daract <goncobos@gmail.com>
2023-03-06 14:46:45 -0300

    acciones edit update delete refactoring

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -18,7 +18,21 @@ class HousesController < ApplicationController
     redirect_to house_path(@house)
   end
 
-  # VOY POR ACAAAAAAAAAAAAAAAAAAAAA
+  def edit
+    @house = House.find(params[:id])
+  end
+
+  def update
+    @house = House.find(params[:id])
+    @house.update(house_params)
+    redirect_to house_path(@house)
+  end
+
+  def destroy
+    @house = House.find(params[:id])
+    @house.destroy
+    redirect_to houses_path, status: :see_other
+  end
 
   private
 
