c4dd11cecf12f339e7f8856aedd5bc955ffd574d
Gonzalo Cobos Daract <goncobos@gmail.com>
2023-03-29 23:24:43 -0300

    Formularios de Sign Up y LogIn hechos

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,5 +1,6 @@
 class HousesController < ApplicationController
   # before_action :set_user, only: %i[new create edit update destroy]
+  skip_before_action :authenticate_user!, only: %i[index show]
   before_action :set_house, only: %i[show edit update destroy]
 
   def index
